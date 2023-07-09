e312ffd6ad9773dfb22b80aa8df96d9e4d1595a9
Rafaelpsp <rafaellpsp@outlook.com>
2023-07-09 20:51:53 -0300

    v2

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -3,6 +3,7 @@ import img1 from '../img/img1.jpg'
 import img2 from '../img/img2.jpg'
 import img3 from '../img/img3.jpg'
 import img4 from '../img/img4.jpg'
+import img5 from '../img/img5.jpg'
 
 const Content = () => {
     return (
@@ -20,12 +21,12 @@ const Content = () => {
                     <p>Seja encontrado pelos seus clientes.</p>
                 </div>
                 <div className="banner-img banner-img-2">
-                <img className="img" src={img2} alt="img2" />
+                    <img className="img" src={img2} alt="img2" />
                 </div>
             </div>
             <div className="banner banner-3">
                 <div className="banner-img banner-img-3">
-                <img className="img" src={img3} alt="img3" />
+                    <img className="img" src={img3} alt="img3" />
                 </div>
                 <div className="banner-description">
                     <p>Nosso time é o seu time.</p>
@@ -36,7 +37,23 @@ const Content = () => {
                     <p>Comece agora.</p>
                 </div>
                 <div className="banner-img banner-img-4">
-                <img className="img" src={img4} alt="img4" />
+                    <img className="img" src={img4} alt="img4" />
+                </div>
+            </div>
+            <div id='teste' className=" banner card-fale-conosco">
+                <div className="banner-img">
+                    <img className="img" src={img5} alt="img5" />
+                </div>
+                <div className="card-form">
+                    <div className="card-text-form-fale-conosco">
+                        <h2>Fale Conosco</h2>
+                    </div>
+                    <form className='form-fale-conosco'>
+                        <input className='fc-input' type="text" placeholder='Nome' />
+                        <input className='fc-input' type="email" placeholder='E-mail' />
+                        <textarea className='fc-textearea' type="text" name="" id="" cols="30" rows="10"></textarea>
+                        <button className='fc-btn' type='submit'>Enviar</button>
+                    </form>
                 </div>
             </div>
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,29 @@
 import './styles/Navbar.css'
 const Navbar = () => {
+
+    const home = ()=>{
+        let obj =document.getElementById("home").scrollIntoView();
+    }
+
+    const sobre = ()=>{
+        let obj =document.getElementById("sobre").scrollIntoView();
+    }
+
+    const contato = ()=>{
+        let obj =document.getElementById("teste").scrollIntoView();
+    }
+
     return (
-        <header className="header">
+        <header id='home' className="header">
             <div className='card-logo'>
                 <h3>WEBSITE</h3>
             </div>
             <div className="card-nav">
                 <div className="card-menu">
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Contato</a></li>
-                        <li><a href="#">Sobre</a></li>
+                        <li><a onClick={home}>Home</a></li>
+                        <li><a onClick={contato}>Contato</a></li>
+                        <li><a onClick={sobre}>Sobre</a></li>
                     </ul>
                 </div>
             </div>
